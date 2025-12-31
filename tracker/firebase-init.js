39c6fb72bbeb24fe8e115c6278fbb62da57cb5d6
imran786sd <37811409+imran786sd@users.noreply.github.com>
2026-01-01 03:39:56 +0530

    Update firebase-init.js

--- tracker/firebase-init.js
+++ tracker/firebase-init.js
@@ -1,6 +1,6 @@
 // ✅ FIXED: Using a valid, stable Firebase version (10.13.1)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged,signInAnonymously } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 // Expose Firebase functions to global scope
@@ -14,7 +14,8 @@ window.firebaseModules = {
     getFirestore, 
     doc, 
     getDoc, 
-    setDoc 
+    setDoc,
+    signInAnonymously
 };
 
 // Dispatch event to signal Firebase is ready
