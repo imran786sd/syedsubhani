d5ac7b85f240753ad37f3484a3e0ad2fe6708a1c
imran786sd <37811409+imran786sd@users.noreply.github.com>
2026-01-01 16:24:02 +0530

    Create config.js

---
+++ The_Ultimate_Gym_2.0/config.js
@@ -0,0 +1,9 @@
+// --- FIREBASE CONFIGURATION ---
+const firebaseConfig = {
+    apiKey: "AIzaSyBg10pA9GtC56C3XR4Fg4fxSzfJbdE83_U",
+    authDomain: "mahira-d7381.firebaseapp.com",
+    projectId: "mahira-d7381",
+    storageBucket: "mahira-d7381.firebasestorage.app",
+    messagingSenderId: "133863322272",
+    appId: "1:133863322272:web:b9e1ff1b4662a74b4f0fc5"
+};
